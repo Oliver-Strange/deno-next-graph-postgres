f3070451155b6ed688f1354c779a684a89d73939
OliverStrange <preston.burton@gmail.com>
2020-10-01 00:11:57 -0700

    sets up the beginnings of user query

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,1 +1,31 @@
 import { applyGraphQL, gql } from "https://deno.land/x/oak_graphql/mod.ts";
+
+// Graphql Type
+const typeDefs = (gql as any)`
+    type User {
+        username: String!,
+        email: String!,
+        password: String!
+    }
+
+    type Query {
+        users: [User]!
+    }
+
+    type Mutation {
+        signup(username: String!, email: String!, password: String!): User
+    }
+`;
+const users = [
+  { username: "preston", email: "test@test.com", password: "1234" },
+];
+
+// Resolvers
+const resolvers = {
+  Query: {
+    users: () => {},
+  },
+  Mutation: {
+    signup: () => {},
+  },
+};
